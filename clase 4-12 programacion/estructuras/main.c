726457af33fba68b233f55ba971cde960c03559d
juliansbaglia18 <47386056+juliansbaglia18@users.noreply.github.com>
2019-05-02 10:08:52 -0300

    Add files via upload

---
+++ clase 4-12 programacion/estructuras/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct eAlumno//la e es porque es una estructura
+{
+    char nombre[50];
+    char apellido[50];
+    int edad;
+    int nota;
+};
+
+int main()
+{
+
+    return 0;
+}
